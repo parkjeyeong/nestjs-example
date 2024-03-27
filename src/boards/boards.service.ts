0d301bb9721472710d4a12a045bfe9db6b70ff4c
parkjeyeong <fullkkk@naver.com>
2024-03-27 23:34:04 +0900

    commit & push

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,6 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { Board, BoardStatus } from './boards.model';
+import { v1 as uuid } from 'uuid';
 
 @Injectable()
 export class BoardsService {
-  
+  private boards: Board[] = [];
+
+  getAllBoards(): Board[] {
+    return this.boards;
+  }
+
+  createBoard(title: string, description: string): Board {
+    const board: Board = {
+      id: uuid(),
+      title: title,
+      description: description,
+      status: BoardStatus.PUBLIC,
+    }
+
+    this.boards.push(board);
+    return board;
+  }
 }
